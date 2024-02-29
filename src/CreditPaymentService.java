79d2926da42200ecf7c1608ca967081f5f43fe59
Дамба Манджиев <mdamba2020@yandex.ru>
2024-02-29 14:21:01 +0300

    create local variable into src/CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public int calculate(int amount, double percent, int year) {
-        year = year * 12;
+        int perYear = year * 12;
         double perMonth = (percent / 12) / 100;
-        double monthly = amount * (perMonth / (Math.pow(1 + perMonth, year) - 1) + perMonth);
+        double monthly = amount * (perMonth / (Math.pow(1 + perMonth, perYear) - 1) + perMonth);
         int payment = (int) monthly;
         return payment;
     }
